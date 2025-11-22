8e8bee5f55afeef85dceae45f35e7bf2fa096e6e
Carlos Tomazin <carlos.tomazin2@gmail.com>
2025-11-22 16:52:18 -0300

    cors

--- app/main.py
+++ app/main.py
@@ -230,7 +230,7 @@ app = FastAPI(title="Football Games API")
 
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
