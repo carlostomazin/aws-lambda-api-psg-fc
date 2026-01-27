d63917f4ab13486f647fadfd2b4d5e021bec670c
Carlos Tomazin <65822302+carlostomazin@users.noreply.github.com>
2026-01-27 03:14:33 +0000

    refatora validações e lógica de inserção/atualização de jogadores no jogo

--- app/src/services/game_player_service.py
+++ app/src/services/game_player_service.py
@@ -3,7 +3,7 @@ from typing import Optional
 from pydantic import BaseModel
 from src.repositories import GamePlayerRepository
 from src.schemas import GamePlayerTeamResponse
-from src.services.game_service import ExceptionGameNotFound, GameService
+from src.services.game_service import GameService
 from src.services.player_service import PlayerService
 
 
@@ -59,65 +59,56 @@ class GamePlayerService:
         return self.repository.get_by_player_id(game_id, player_id)
 
     def update_player_in_game(self, game_id, player_id, data: GamePlayerUpdateSchema):
-        player_service = PlayerService()
+        # Validações
+        if data.paid is True and data.amount_paid is None:
+            raise Exception("O valor pago deve ser informado quando o jogador for marcado como pago.")
 
-        # Recupera ou cria o jogador que convidou, se aplicável
-        invited_by_id = None
-        if data.is_visitor is True:
-            if data.invited_by:
-                invited_by_id = player_service.resolve_or_create_player(
-                    data.invited_by
-                ).id
-            else:
-                raise Exception("O jogador visitante deve ter um convidador.")
-        
+        if data.is_visitor is True and data.invited_by is None:
+            raise Exception("O jogador visitante deve ter um convidador.")
+
         # Prepara os dados para atualização
+        player_service = PlayerService()
         update_data = {}
 
-        if data.is_goalkeeper is not None:
+        if data.is_goalkeeper:
             update_data["is_goalkeeper"] = data.is_goalkeeper
-        if data.is_visitor is not None:
+        if data.is_visitor:
             update_data["is_visitor"] = data.is_visitor
-        if invited_by_id is not None or data.invited_by is not None:
-            update_data["invited_by"] = invited_by_id
-        if data.paid is not None:
+        if data.invited_by:
+            update_data["invited_by"] = player_service.get_or_create_player_by_name(data.invited_by).id
+        if data.paid:
             update_data["paid"] = data.paid
-        if data.amount_paid is not None:
+        if data.amount_paid:
             update_data["amount_paid"] = data.amount_paid
-        if data.team is not None:
+        if data.team:
             update_data["team"] = data.team
 
         return self.repository.update(game_id, player_id, update_data)
 
     def add_player_in_game(self, game_id: str, data: GamePlayerAddSchema):
-        # Validar se o game_id existe
+        # Validações
         game_service = GameService()
         if not game_service.get_game_by_id(game_id):
-            raise ExceptionGameNotFound
+            raise Exception("Jogo não encontrado.")
 
-        # Recupera ou cria o jogador
+        if data.paid is True and data.amount_paid is None:
+            raise Exception("O valor pago deve ser informado quando o jogador for marcado como pago.")
+
+        if data.is_visitor is True and data.invited_by is None:
+            raise Exception("O jogador visitante deve ter um convidador.")
+
+        # Preparar os dados para inserção
         player_service = PlayerService()
-        player_id = player_service.resolve_or_create_player(data.name).id
 
-        # Recupera ou cria o jogador que convidou, se aplicável
-        invited_by_id = None
-        if data.is_visitor is True:
-            if data.invited_by:
-                invited_by_id = player_service.resolve_or_create_player(
-                    data.invited_by
-                ).id
-            else:
-                raise Exception("O jogador visitante deve ter um convidador.")
-
-        # Inserir ou atualizar o jogador no jogo
-        data = {
+        add_data = {
             "game_id": game_id,
-            "player_id": player_id,
+            "player_id": player_service.get_or_create_player_by_name(data.name).id,
             "is_goalkeeper": data.is_goalkeeper,
             "is_visitor": data.is_visitor,
-            "invited_by": invited_by_id,
+            "invited_by": player_service.get_or_create_player_by_name(data.invited_by).id if data.invited_by else None,
             "paid": data.paid,
+            "amount_paid": data.amount_paid,
             "team": data.team,
         }
 
-        self.repository.upsert(data)
+        self.repository.upsert(add_data)
