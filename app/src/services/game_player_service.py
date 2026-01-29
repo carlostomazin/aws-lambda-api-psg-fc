5df59ca3d03ba65af85be9e0b067763e1a8fb36c
Carlos Tomazin <carlos.tomazin2@gmail.com>
2026-01-28 22:09:58 -0300

    remove 'paid' field from GamePlayer schemas and related logic in service methods

--- app/src/services/game_player_service.py
+++ app/src/services/game_player_service.py
@@ -10,7 +10,6 @@ class GamePlayerAddSchema(BaseModel):
     is_goalkeeper: Optional[bool] = False
     is_visitor: Optional[bool] = False
     invited_by: Optional[str] = None
-    paid: Optional[bool] = False
     amount_paid: Optional[float] = None
     team: Optional[str] = None
 
@@ -19,7 +18,6 @@ class GamePlayerUpdateSchema(BaseModel):
     is_goalkeeper: Optional[bool] = None
     is_visitor: Optional[bool] = None
     invited_by: Optional[str] = None
-    paid: Optional[bool] = None
     amount_paid: Optional[float] = None
     team: Optional[str] = None
 
@@ -60,11 +58,6 @@ class GamePlayerService:
 
     def update_player_in_game(self, game_id, player_id, data: GamePlayerUpdateSchema):
         # Regras de negócio
-        if data.paid is True and data.amount_paid is None:
-            raise Exception("O valor pago deve ser informado quando o jogador for marcado como pago.")
-        else:
-            pass
-
         if data.is_visitor is True and data.invited_by is None:
             raise Exception("O jogador visitante deve ter um convidador.")
 
@@ -78,8 +71,6 @@ class GamePlayerService:
             update_data["is_visitor"] = data.is_visitor
         if data.invited_by:
             update_data["invited_by"] = player_service.get_or_create_player(data.invited_by).id
-        if data.paid:
-            update_data["paid"] = data.paid
         if data.amount_paid:
             update_data["amount_paid"] = data.amount_paid
         if data.team:
@@ -89,9 +80,6 @@ class GamePlayerService:
 
     def add_player_in_game(self, game_id: str, data: GamePlayerAddSchema):
         # Regras de negócio
-        if data.paid is True and data.amount_paid is None:
-            raise Exception("O valor pago deve ser informado quando o jogador for marcado como pago.")
-
         if data.is_visitor is True and data.invited_by is None:
             raise Exception("O jogador visitante deve ter um convidador.")
 
@@ -104,7 +92,6 @@ class GamePlayerService:
             "is_goalkeeper": data.is_goalkeeper,
             "is_visitor": data.is_visitor,
             "invited_by": (player_service.get_or_create_player(data.invited_by).id if data.invited_by else None),
-            "paid": data.paid,
             "amount_paid": data.amount_paid,
             "team": data.team,
         }
