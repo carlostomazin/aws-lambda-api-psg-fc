80e37fcb8f85492709d5951af96d1d01de429097
Carlos Tomazin <carlos.tomazin2@gmail.com>
2026-01-28 22:04:22 -0300

    refatora PlayerRepository para remover métodos não utilizados e melhorar a lógica de criação e atualização de jogadores

--- app/src/repositories/player_repository.py
+++ app/src/repositories/player_repository.py
@@ -6,10 +6,17 @@ class PlayerRepository:
     def __init__(self):
         self.supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-    def get_all(self) -> list[dict] | None:
-        response = self.supabase.table("players").select("*").order("name").execute()
+    def create(self, body: dict) -> dict | None:
+        response = self.supabase.table("players").insert(body).execute()
         if response.data:
-            return response.data
+            return response.data[0]
+        return None
+
+    def update(self, player_id: str, body: dict) -> dict | None:
+        """Update player data in Supabase"""
+        response = self.supabase.table("players").update(body).eq("id", player_id).execute()
+        if response.data:
+            return response.data[0]
         return None
 
     def get(self, filters: dict | None = None) -> list[dict] | None:
@@ -26,44 +33,11 @@ class PlayerRepository:
                 else:
                     query = query.eq(field, value)
 
+        # order
+        query = query.order("name")
+
         response = query.execute()
         return response.data or None
-
-    def get_by_id(self, player_id: str) -> dict | None:
-        response = (
-            self.supabase.table("players").select("*").eq("id", player_id).execute()
-        )
-        if response.data:
-            return response.data[0]
-        return None
-
-    def get_by_name(self, player_name: str) -> dict | None:
-        response = (
-            self.supabase.table("players")
-            .select("*")
-            .ilike("name", player_name)
-            .limit(1)
-            .execute()
-        )
-
-        if response.data:
-            return response.data[0]
-        return None
-
-    def create(self, body: dict) -> dict | None:
-        response = self.supabase.table("players").insert(body).execute()
-        if response.data:
-            return response.data[0]
-        return None
-
-    def update(self, player_id: str, body: dict) -> dict | None:
-        """Update player data in Supabase"""
-        response = (
-            self.supabase.table("players").update(body).eq("id", player_id).execute()
-        )
-        if response.data:
-            return response.data[0]
-        return None
 
     def delete(self, player_id: str) -> None:
         self.supabase.table("players").delete().eq("id", player_id).execute()
