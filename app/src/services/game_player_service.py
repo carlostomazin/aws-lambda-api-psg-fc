27b59c84796452eceaf336e90592720e6724add8
Carlos Tomazin <carlos.tomazin2@gmail.com>
2026-01-30 17:32:46 -0300

    refactor: enhance player update logic with additional validation and error handling

--- app/src/services/game_player_service.py
+++ app/src/services/game_player_service.py
@@ -76,14 +76,21 @@ class GamePlayerService:
         self.repository.upsert(add_data)
 
     def update_player_in_game(self, game_id, player_id, data: GamePlayerUpdateSchema):
+        print("update_player_in_game", data)
         # Regras de negócio
-        if data.paid is True and data.amount_paid is None:
-            raise Exception("Para marcar o jogador como pago, é necessário informar o valor pago.")
-        else:
-            from src.services.game_service import GameService
+        from src.services.game_service import GameService
 
-            game_service = GameService()
-            game = game_service.get_game(game_id)
+        game = GameService().get_game(game_id)
+
+        if data.paid is True and data.amount_paid is None and game["price_per_player"] is None:
+            raise Exception(
+                "Para marcar o jogador como pago, é necessário informar o valor pago ou ter um preço por jogador definido no jogo."
+            )
+
+        if data.is_visitor is True and data.invited_by is None:
+            raise Exception("O jogador visitante deve ter um convidador.")
+
+        if data.paid is True:
             player = self.get_player_in_game(game_id, player_id)
             if data.is_goalkeeper is True or player["is_goalkeeper"] is True:
                 if game["goalkeepers_pay"] is False:
@@ -93,28 +100,29 @@ class GamePlayerService:
                         data.amount_paid = game["price_per_player"]
             elif data.amount_paid == 0.0:
                 if game["price_per_player"] is not None:
+                    print("Setting amount_paid to price_per_player:", game["price_per_player"])
                     data.amount_paid = game["price_per_player"]
-
-        if data.is_visitor is True and data.invited_by is None:
-            raise Exception("O jogador visitante deve ter um convidador.")
 
         # Prepara os dados para atualização
         update_data = {}
 
-        if data.is_goalkeeper:
+        if data.is_goalkeeper is not None:
             update_data["is_goalkeeper"] = data.is_goalkeeper
-        if data.is_visitor:
+        if data.is_visitor is not None:
             update_data["is_visitor"] = data.is_visitor
-        if data.invited_by:
+        if data.invited_by is not None:
             from src.services.player_service import PlayerService
 
             player_service = PlayerService()
             update_data["invited_by"] = player_service.get_or_create_player(data.invited_by).id
-        if data.amount_paid:
-            update_data["amount_paid"] = data.amount_paid
-        if data.team:
+        if data.paid is not None:
+            update_data["paid"] = data.paid
+        if data.amount_paid is not None:
+            update_data["amount_paid"] = data.amount_paid if data.paid else 0.0
+        if data.team is not None:
             update_data["team"] = data.team
 
+        print("update_data", update_data)
         return self.repository.update(game_id, player_id, update_data)
 
     def get_player_in_game(self, game_id: str, player_id: str) -> Optional[dict]:
